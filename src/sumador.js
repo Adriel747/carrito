e37f09b90dab7b115ab0b2c42aa1dc565bb0befb
Adriel Fernandez <adriel.fernandez@ucb.edu.bo>
2023-09-03 23:55:16 -0400

    se le  agrega variable para agregar estructura (auto)

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,14 @@
-function mover(m) {
-  return "0,0N";
+var auto = {
+  dieccion: "N",
+  posx: 0,
+  posy: 0
 }
+function mover(m) {
+if(m == "N"){
+  auto.posy = auto.posy + 1;
+}
+return auto.posx.toString() + ',' + auto.posy.toString() + auto.dieccion
+}
+
 
 export default mover;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,4 +4,9 @@ describe("Sumar", () => {
   it("mostrar posicion inicial 0,N", () => {
     expect(mover(3, 2)).toEqual("0,0N");
   });
+  describe("Mover Auto Norte", () => {
+    it("deberia moverse hacia el norte", () => {
+      expect(mover("N")).toEqual("0,1N");
+    });
+  });
 });
